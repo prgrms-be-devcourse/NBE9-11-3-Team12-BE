0c54f63bec8eeaa1f7b4a9c04a56ed8e49665596
최윤서 <128898996+YunseoChoe@users.noreply.github.com>
2026-05-15 14:28:40 +0900

    [RG-27] refactor: users dto 마이그레이션 오류 수정 및 null 허용 제거
    * [RG-27] refactor: users dto 마이그레이션 오류 수정 및 null 허용 제거
    
    * [RG-27] refactor: users dto @NotNull 어노테이션 제거

--- backend/src/main/java/com/rungo/api/domain/users/dto/CompleteProfileReq.kt
+++ backend/src/main/java/com/rungo/api/domain/users/dto/CompleteProfileReq.kt
@@ -3,7 +3,6 @@ package com.rungo.api.domain.users.dto
 import com.rungo.api.domain.users.enumtype.Gender
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Past
 import jakarta.validation.constraints.Pattern
 import java.time.LocalDate
@@ -12,8 +11,7 @@ import java.time.LocalDate
 data class CompleteProfileReq(
     @field:Schema(description = "사용자 이름", example = "홍길동")
     @field:NotBlank(message = "이름은 필수 입력값입니다.")
-    @JvmField
-    val name: String?,
+    val name: String,
 
     @field:Schema(description = "전화번호", example = "010-1234-5678")
     @field:NotBlank(message = "전화번호는 필수 입력값입니다.")
@@ -21,17 +19,12 @@ data class CompleteProfileReq(
         regexp = "^010-\\d{4}-\\d{4}$",
         message = "전화번호 형식이 올바르지 않습니다. (예: 010-1234-5678)"
     )
-    @JvmField
-    val phoneNumber: String?,
+    val phoneNumber: String,
 
-    @field:Schema(description = "성별", example = "gender")
-    @field:NotNull
-    @JvmField
-    val gender: Gender?,
+    @field:Schema(description = "성별", example = "MALE")
+    val gender: Gender,
 
     @field:Schema(description = "생년월일", example = "1990-01-01")
-    @field:NotNull
     @field:Past(message = "생년월일은 과거 날짜여야 합니다.")
-    @JvmField
-    val birth: LocalDate?
+    val birth: LocalDate
 )
--- backend/src/main/java/com/rungo/api/domain/users/dto/MyProfileRes.kt
+++ backend/src/main/java/com/rungo/api/domain/users/dto/MyProfileRes.kt
@@ -9,29 +9,23 @@ import java.time.LocalDate
 @JvmRecord
 data class MyProfileRes(
     @field:Schema(description = "사용자 ID", example = "1")
-    @JvmField
     val id: Long,
 
     @field:Schema(description = "사용자 이메일", example = "user@test.com")
-    @JvmField
     val email: String,
 
     @field:Schema(description = "사용자 이름", example = "홍길동")
-    @JvmField
     val name: String,
 
     @field:Schema(description = "전화번호", example = "010-1234-5678")
-    @JvmField
     val phoneNumber: String,
 
     @field:Schema(description = "성별", example="MALE")
-    @JvmField
     val gender: Gender,
 
     @field:Schema(description = "생년월일", example = "1990-01-01")
     val birth: LocalDate,
 
     @field:Schema(description = "사용자 역할", example = "USER")
-    @JvmField
     val role: Role
 ) 
