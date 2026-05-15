1afad2cebcd37a6488bf951a4611fa38b659e566
choiyunseo <choeyunseo4060@naver.com>
2026-05-15 17:08:52 +0900

    [RG-42] refactor: findByIdOrNull 적용

--- backend/src/main/java/com/rungo/api/domain/users/service/UsersService.kt
+++ backend/src/main/java/com/rungo/api/domain/users/service/UsersService.kt
@@ -8,16 +8,17 @@ import com.rungo.api.domain.users.repository.UserRepository
 import com.rungo.api.global.exception.CustomException
 import com.rungo.api.global.exception.ErrorCode
 import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class UsersService(
-    private val usersRepository: UserRepository
+    private val userRepository: UserRepository
 ) {
 
     fun getMyInfo(userId: Long): MyProfileRes {
-        val user = usersRepository.findById(userId)
-            .orElseThrow { CustomException(ErrorCode.USER_NOT_FOUND) }
+        val user = userRepository.findByIdOrNull(userId)
+            ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
 
         return MyProfileRes(
             user.id,
@@ -32,8 +33,8 @@ class UsersService(
 
     @Transactional
     fun updateMyProfile(userId: Long, req: UpdateMyProfileReq): UpdateMyProfileRes {
-        val user = usersRepository.findById(userId)
-            .orElseThrow { CustomException(ErrorCode.USER_NOT_FOUND) }
+        val user = userRepository.findByIdOrNull(userId)
+            ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
 
         user.updateProfile(
             req.name ?: user.name,
@@ -55,8 +56,8 @@ class UsersService(
 
     @Transactional
     fun completeMyProfile(userId: Long, req: CompleteProfileReq) {
-        val user = usersRepository.findById(userId)
-            .orElseThrow { CustomException(ErrorCode.USER_NOT_FOUND) }
+        val user = userRepository.findByIdOrNull(userId)
+            ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
 
         user.updateProfile(
             req.name,
