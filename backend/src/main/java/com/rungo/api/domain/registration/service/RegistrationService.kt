e4ae5cc9e25e5310b5216715546b2d06181967e8
K-Dho <kdongho0406@gmail.com>
2026-05-15 14:32:21 +0900

    [RG-36] refactor: registration service 코틀린 마이그레이션

--- backend/src/main/java/com/rungo/api/domain/registration/service/RegistrationService.kt
+++ backend/src/main/java/com/rungo/api/domain/registration/service/RegistrationService.kt
@@ -7,7 +7,6 @@ import com.rungo.api.domain.marathon.marathon.repository.MarathonRepository
 import com.rungo.api.domain.notification.event.RegistrationCompletedEvent
 import com.rungo.api.domain.registration.dto.CreateRegistrationReq
 import com.rungo.api.domain.registration.dto.CreateRegistrationRes
-import com.rungo.api.domain.registration.dto.CreateRegistrationRes.Companion.from
 import com.rungo.api.domain.registration.dto.MyRegistrationRes
 import com.rungo.api.domain.registration.entity.Registration
 import com.rungo.api.domain.registration.entity.RegistrationCancelHistory
@@ -17,50 +16,45 @@ import com.rungo.api.domain.registration.repository.RegistrationRepository
 import com.rungo.api.domain.users.repository.UserRepository
 import com.rungo.api.global.exception.CustomException
 import com.rungo.api.global.exception.ErrorCode
-import lombok.RequiredArgsConstructor
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
-import java.util.function.Function
-import java.util.function.Supplier
-import java.util.stream.Collectors
 
 @Service
-@RequiredArgsConstructor
 @Transactional
-class RegistrationService {
-    private val registrationRepository: RegistrationRepository? = null
-    private val registrationCancelHistoryRepository: RegistrationCancelHistoryRepository? = null
-    private val courseRepository: CourseRepository? = null
-    private val marathonRepository: MarathonRepository? = null
-    private val userRepository: UserRepository? = null
-    private val eventPublisher: ApplicationEventPublisher? = null
-
+class RegistrationService(
+    private val registrationRepository: RegistrationRepository,
+    private val registrationCancelHistoryRepository: RegistrationCancelHistoryRepository,
+    private val courseRepository: CourseRepository,
+    private val marathonRepository: MarathonRepository,
+    private val userRepository: UserRepository,
+    private val eventPublisher: ApplicationEventPublisher,
+) {
     fun create(userId: Long, request: CreateRegistrationReq): CreateRegistrationRes {
         // 필수 약관 미동의
-
         if (!request.agreedTerms) {
             throw CustomException(ErrorCode.REGISTRATION_TERMS_REQUIRED)
         }
 
-        val user = userRepository!!.findById(userId)
-            .orElseThrow<CustomException?>(Supplier { CustomException(ErrorCode.USER_NOT_FOUND) })
+        val user = userRepository.findByIdOrNull(userId)
+            ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
 
-        if (!user.isProfileCompleted()) {
+        if (!user.isProfileCompleted) {
             throw CustomException(ErrorCode.PROFILE_NOT_COMPLETED)
         }
 
-        val course = courseRepository!!.findById(request.courseId)
-            .orElseThrow<CustomException?>(Supplier { CustomException(ErrorCode.COURSE_NOT_FOUND) })
-        val marathon = course.getMarathon()
+        val course = courseRepository.findByIdOrNull(request.courseId)
+            ?: throw CustomException(ErrorCode.COURSE_NOT_FOUND)
+        val marathon = course.marathon
         val now = LocalDateTime.now()
 
         // 접수 기간이 아니면 생성할 수 없다.
-        if (now.isBefore(marathon.getRegistrationStartAt()) || now.isAfter(marathon.getRegistrationEndAt())) {
+        if (now.isBefore(marathon.registrationStartAt) || now.isAfter(marathon.registrationEndAt)) {
             throw CustomException(ErrorCode.REGISTRATION_PERIOD_INVALID)
         }
         // 모집 중인 대회만 접수 가능하다.
@@ -83,40 +77,41 @@ class RegistrationService {
             request.agreedTerms
         )
 
-        val updatedRows = courseRepository.increaseCurrentCountIfNotFull(course.getId())
+        val updatedRows = courseRepository.increaseCurrentCountIfNotFull(course.id)
         // 코스 정원이 가득 찼으면 접수를 막는다.
         if (updatedRows == 0) {
             throw CustomException(ErrorCode.CAPACITY_FULL)
         }
         // 동시성 제어 미적용 메서드
         // course.increaseCurrentCount();
-        val savedRegistration = registrationRepository!!.save<Registration>(registration)
+        val savedRegistration = registrationRepository.save(registration)
 
-        eventPublisher!!.publishEvent(
+        eventPublisher.publishEvent(
             RegistrationCompletedEvent(
-                user.getEmail(),
-                marathon.getTitle(),
-                course.getCourseType()
+                user.email,
+                marathon.title,
+                course.courseType
             )
         )
 
-        return from(savedRegistration)
+        return CreateRegistrationRes.from(savedRegistration)
     }
 
-    fun cancel(userId: Long?, registrationId: Long) {
-        val registration = registrationRepository!!.findById(registrationId) // 존재하지 않는 접수 건은 취소할 수 없다.
-            .orElseThrow<CustomException?>(Supplier { CustomException(ErrorCode.REGISTRATION_NOT_FOUND) })
+    fun cancel(userId: Long, registrationId: Long) {
+        val registration = registrationRepository.findByIdOrNull(registrationId)
+            // 존재하지 않는 접수 건은 취소할 수 없다.
+            ?: throw CustomException(ErrorCode.REGISTRATION_NOT_FOUND)
 
         // 본인 신청 건만 취소할 수 있다.
-        if (registration.getUser().getId() != userId) {
+        if (registration.user.id != userId) {
             throw CustomException(ErrorCode.FORBIDDEN)
         }
 
-        val marathon = registration.getMarathon()
+        val marathon = registration.marathon
 
         val now = LocalDateTime.now()
         // 접수 마감 이후에는 취소할 수 없다.
-        if (now.isAfter(marathon.getRegistrationEndAt())) {
+        if (now.isAfter(marathon.registrationEndAt)) {
             throw CustomException(ErrorCode.REGISTRATION_CANCEL_PERIOD_INVALID)
         }
         // 모집 중인 대회만 취소할 수 있다.
@@ -128,13 +123,11 @@ class RegistrationService {
             throw CustomException(ErrorCode.MARATHON_ALREADY_CANCELED)
         }
         //유니크 제약을 바로 확인하기 위해 saveAndFlush
-        registrationCancelHistoryRepository!!.saveAndFlush<RegistrationCancelHistory?>(
-            RegistrationCancelHistory.create(
-                registration
-            )
+        registrationCancelHistoryRepository.saveAndFlush(
+            RegistrationCancelHistory.create(registration)
         )
 
-        courseRepository!!.decreaseCurrentCountIfPositive(registration.getCourse().getId())
+        courseRepository.decreaseCurrentCountIfPositive(registration.course.id)
         // 동시성 제어 미적용 메서드
         // registration.getCourse().decreaseCurrentCount();
         registrationRepository.delete(registration)
@@ -144,56 +137,51 @@ class RegistrationService {
     // ACTIVE   : 정상 접수인 상태 (취소 되지 않은 모든 접수)
     // CANCELED : 취소된 접수 상태
     @Transactional(readOnly = true)
-    fun getMyRegistrations(userId: Long?, status: MyRegistrationStatusFilter?, pageable: Pageable): MyRegistrationRes {
-        if (status == MyRegistrationStatusFilter.CANCELED) {
-            return getCanceledRegistrations(userId, pageable)
-        }
-
-        return getActiveRegistrations(userId, pageable)
+    fun getMyRegistrations(userId: Long, status: MyRegistrationStatusFilter, pageable: Pageable,
+    ): MyRegistrationRes = when (status) {
+        MyRegistrationStatusFilter.ACTIVE -> getActiveRegistrations(userId, pageable)
+        MyRegistrationStatusFilter.CANCELED -> getCanceledRegistrations(userId, pageable)
     }
 
-    private fun getActiveRegistrations(userId: Long?, pageable: Pageable): MyRegistrationRes {
+    private fun getActiveRegistrations(userId: Long, pageable: Pageable): MyRegistrationRes {
         val sortedPageable: Pageable = PageRequest.of(
-            pageable.getPageNumber(),
-            pageable.getPageSize(),
+            pageable.pageNumber,
+            pageable.pageSize,
             Sort.by(
                 Sort.Order.desc("appliedAt"),
                 Sort.Order.desc("id")
             )
         )
-
-        val page = registrationRepository!!.findByUser_Id(userId, sortedPageable)
+        val page = registrationRepository.findByUser_Id(userId, sortedPageable)
 
         return MyRegistrationRes.fromActive(page)
     }
 
-    private fun getCanceledRegistrations(userId: Long?, pageable: Pageable): MyRegistrationRes {
+    private fun getCanceledRegistrations(userId: Long, pageable: Pageable): MyRegistrationRes {
         val sortedPageable: Pageable = PageRequest.of(
-            pageable.getPageNumber(),
-            pageable.getPageSize(),
+            pageable.pageNumber,
+            pageable.pageSize,
             Sort.by(
                 Sort.Order.desc("canceledAt"),
                 Sort.Order.desc("id")
             )
         )
 
-        val page = registrationCancelHistoryRepository!!.findByUserId(userId, sortedPageable)
+        val page = registrationCancelHistoryRepository.findByUserId(userId, sortedPageable)
 
-        val marathonIds = page.getContent().stream()
-            .map<Long?> { obj: RegistrationCancelHistory? -> obj!!.getMarathonId() }
-            .distinct()
-            .toList()
+        val marathonIds = page.content
+            .map { it.marathonId }
+            .toSet()
 
-        val courseIds = page.getContent().stream()
-            .map<Long?> { obj: RegistrationCancelHistory? -> obj!!.getCourseId() }
-            .distinct()
-            .toList()
+        val courseIds = page.content
+            .map { it.courseId }
+            .toSet()
 
-        val marathonMap = marathonRepository!!.findAllById(marathonIds).stream()
-            .collect(Collectors.toMap(Function { obj: Marathon? -> obj!!.getId() }, Function.identity<Marathon?>()))
+        val marathonMap: Map<Long, Marathon> = marathonRepository.findAllById(marathonIds)
+            .associateBy { it.id }
 
-        val courseMap = courseRepository!!.findAllById(courseIds).stream()
-            .collect(Collectors.toMap(Function { obj: Course? -> obj!!.getId() }, Function.identity<Course?>()))
+        val courseMap: Map<Long, Course> = courseRepository.findAllById(courseIds)
+            .associateBy { it.id }
 
         return MyRegistrationRes.fromCanceled(page, marathonMap, courseMap)
     }
