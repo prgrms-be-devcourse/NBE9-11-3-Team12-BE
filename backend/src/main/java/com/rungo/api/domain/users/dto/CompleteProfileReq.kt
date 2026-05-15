804bc334f0ee36bdd4232434ae7dbac28e360038
choiyunseo <choeyunseo4060@naver.com>
2026-05-15 15:25:53 +0900

    refactor: [RG-27] PatchMapping에 맞게 DTO null 허용 수정

--- backend/src/main/java/com/rungo/api/domain/users/dto/CompleteProfileReq.kt
+++ backend/src/main/java/com/rungo/api/domain/users/dto/CompleteProfileReq.kt
@@ -11,7 +11,7 @@ import java.time.LocalDate
 data class CompleteProfileReq(
     @field:Schema(description = "사용자 이름", example = "홍길동")
     @field:NotBlank(message = "이름은 필수 입력값입니다.")
-    val name: String,
+    val name: String?,
 
     @field:Schema(description = "전화번호", example = "010-1234-5678")
     @field:NotBlank(message = "전화번호는 필수 입력값입니다.")
@@ -19,12 +19,12 @@ data class CompleteProfileReq(
         regexp = "^010-\\d{4}-\\d{4}$",
         message = "전화번호 형식이 올바르지 않습니다. (예: 010-1234-5678)"
     )
-    val phoneNumber: String,
+    val phoneNumber: String?,
 
     @field:Schema(description = "성별", example = "MALE")
-    val gender: Gender,
+    val gender: Gender?,
 
     @field:Schema(description = "생년월일", example = "1990-01-01")
     @field:Past(message = "생년월일은 과거 날짜여야 합니다.")
-    val birth: LocalDate
+    val birth: LocalDate?
 )
--- backend/src/main/java/com/rungo/api/domain/users/dto/UpdateMyProfileReq.kt
+++ backend/src/main/java/com/rungo/api/domain/users/dto/UpdateMyProfileReq.kt
@@ -2,9 +2,9 @@ package com.rungo.api.domain.users.dto
 
 import com.rungo.api.domain.users.enumtype.Gender
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Past
 import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
 import java.time.LocalDate
 
 @Schema(description = "내 정보 수정 요청 DTO")
@@ -12,21 +12,20 @@ import java.time.LocalDate
 data class UpdateMyProfileReq(
 
     @field:Schema(description = "사용자 이름", example = "홍길동")
-    @field:NotBlank(message = "이름은 필수 입력값입니다.")
-    val name: String,
+    @field:Size(min = 1, message = "이름은 1자 이상이어야 합니다.") // null이 아닌 ""가 들어갔을 경우
+    val name: String?,
 
     @field:Schema(description = "전화번호", example = "010-1234-5678")
-    @field:NotBlank(message = "전화번호는 필수 입력값입니다.")
     @field:Pattern(
         regexp = "^010-\\d{4}-\\d{4}$",
         message = "전화번호 형식이 올바르지 않습니다. (예: 010-1234-5678)"
     )
-    val phoneNumber: String,
+    val phoneNumber: String?,
 
     @field:Schema(description = "성별", example = "MALE")
-    val gender: Gender,
+    val gender: Gender?,
 
     @field:Schema(description = "생년월일", example = "1990-01-01")
     @field:Past(message = "생년월일은 과거 날짜여야 합니다.")
-    val birth: LocalDate,
+    val birth: LocalDate?,
 )
