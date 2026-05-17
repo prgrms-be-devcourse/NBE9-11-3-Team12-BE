ec73ecfa5d89e376ef3b2c9166fd0bb12cbc5384
MMuhwon <mwon1590@gmail.com>
2026-05-17 22:29:23 +0900

    [RG-46] refacfor: OAuth2AuthenticationFailureHandler kotlin migration

--- backend/src/main/java/com/rungo/api/domain/auth/handler/OAuth2AuthenticationFailureHandler.kt
+++ backend/src/main/java/com/rungo/api/domain/auth/handler/OAuth2AuthenticationFailureHandler.kt
@@ -1,20 +1,18 @@
 package com.rungo.api.domain.auth.handler
 
-import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.authentication.AuthenticationFailureHandler
 import org.springframework.stereotype.Component
-import java.io.IOException
 
 @Component
 class OAuth2AuthenticationFailureHandler : AuthenticationFailureHandler {
-    @Throws(IOException::class, ServletException::class)
+
     override fun onAuthenticationFailure(
-        request: HttpServletRequest?,
+        request: HttpServletRequest,
         response: HttpServletResponse,
-        exception: AuthenticationException?
+        exception: AuthenticationException
     ) {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "OAuth2 Login Failed")
     }
