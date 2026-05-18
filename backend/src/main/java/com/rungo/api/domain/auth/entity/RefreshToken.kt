c31f262b5f36e953eb9c96c54f8e7db39117ae3e
MMuhwon <mwon1590@gmail.com>
2026-05-18 15:50:37 +0900

    [RG-53] refactor: RefreshToken data class 수정

--- backend/src/main/java/com/rungo/api/domain/auth/entity/RefreshToken.kt
+++ backend/src/main/java/com/rungo/api/domain/auth/entity/RefreshToken.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 
 @RedisHash(value = "refresh", timeToLive = 60 * 60 * 24 * 7)
-class RefreshToken(
+data class RefreshToken(
     @Id
     val userId: Long,
     val refreshToken: String
