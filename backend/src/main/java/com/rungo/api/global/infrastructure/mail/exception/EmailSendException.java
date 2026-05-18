5d89355ed1b62ccd8d098e7a50d8fae16eafa89b
장재원 <49019307+MMuhwon@users.noreply.github.com>
2026-05-18 22:32:39 +0900

    [RG-63] refactor: infrastructure.mail 코틀린 마이그레이션
    * [RG-63] refactor: EmailMessage kotlin migration
    
    * [RG-63] refactor: EmailSenderClient kotlin migration
    
    * [RG-63] refactor: EmailService kotlin migration
    
    * [RG-63] refactor: EmailSendException kotlin migration
    
    * [RG-63] refactor: 테스트코드 에러코드 401로 수정, kotlin nullable 반영
    
    * [RG-63] refactor: EmailSenderClient 생성자 주입 도입

--- backend/src/main/java/com/rungo/api/global/infrastructure/mail/exception/EmailSendException.java
+++
@@ -1,8 +0,0 @@
-package com.rungo.api.global.infrastructure.mail.exception;
-
-public class EmailSendException extends RuntimeException {
-
-    public EmailSendException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
