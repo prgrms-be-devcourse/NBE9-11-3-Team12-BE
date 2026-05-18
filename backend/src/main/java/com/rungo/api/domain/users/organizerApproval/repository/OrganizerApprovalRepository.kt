e9a4b337817ad76e4b118afd22acad969eec8cd9
ksg1103 <ksgwkd1103@naver.com>
2026-05-18 20:44:10 +0900

    [RG-60] refactor:repository nun-null하도록

--- backend/src/main/java/com/rungo/api/domain/users/organizerApproval/repository/OrganizerApprovalRepository.kt
+++ backend/src/main/java/com/rungo/api/domain/users/organizerApproval/repository/OrganizerApprovalRepository.kt
@@ -3,4 +3,4 @@ package com.rungo.api.domain.users.organizerApproval.repository
 import com.rungo.api.domain.users.organizerApproval.entity.OrganizerApproval
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface OrganizerApprovalRepository : JpaRepository<OrganizerApproval?, Long?>
+interface OrganizerApprovalRepository : JpaRepository<OrganizerApproval, Long>
