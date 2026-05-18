90a2e5026ea084f950ab62364f37d2fd9f40b0f0
MMuhwon <mwon1590@gmail.com>
2026-05-19 00:03:19 +0900

    [RG-71] refactor: notification JVM 어노테이션 제거

--- backend/src/main/java/com/rungo/api/domain/notification/event/RegistrationCompletedEvent.kt
+++ backend/src/main/java/com/rungo/api/domain/notification/event/RegistrationCompletedEvent.kt
@@ -1,6 +1,5 @@
 package com.rungo.api.domain.notification.event
 
-@JvmRecord // 기존 자바 코드에서 event.email()로 접근 가능
 data class RegistrationCompletedEvent(
     val email: String,
     val marathonTitle: String,
