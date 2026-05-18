5050ed0aa9fa5de04433e720e90ff98ea53f1b7c
ksg1103 <ksgwkd1103@naver.com>
2026-05-18 21:11:56 +0900

    [RG-60] refactor: organizerApproval 도메인 마이그레이션
    * [RG-60] refactor:organizer 마이그레이션
    
    * [RG-60] refactor:repository nun-null하도록

--- backend/src/main/java/com/rungo/api/domain/users/organizerApproval/repository/OrganizerApprovalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rungo.api.domain.users.organizerApproval.repository;
-
-import com.rungo.api.domain.users.organizerApproval.entity.OrganizerApproval;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizerApprovalRepository extends JpaRepository<OrganizerApproval,Long> {
-}
---
+++ backend/src/main/java/com/rungo/api/domain/users/organizerApproval/status/ApproveStatus.kt
@@ -0,0 +1,7 @@
+package com.rungo.api.domain.users.organizerApproval.status
+
+enum class ApproveStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
