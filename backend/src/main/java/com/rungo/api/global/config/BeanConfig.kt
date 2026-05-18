b2af263e6b90c4fce145e76de70eacf0a3216d6e
K-Dho <kdongho0406@gmail.com>
2026-05-18 14:30:45 +0900

    [RG-55] refactor: BeanConfig 코틀린 전환

---
+++ backend/src/main/java/com/rungo/api/global/config/BeanConfig.kt
@@ -0,0 +1,12 @@
+package com.rungo.api.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class BeanConfig {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder(6)
+}
