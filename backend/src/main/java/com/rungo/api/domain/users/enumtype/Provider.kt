f8b02eb86c339e7e61c27d779b995308cebf2c09
MMuhwon <mwon1590@gmail.com>
2026-05-18 14:17:26 +0900

    [RG-53] refactor: users enumtype kotlin migration

---
+++ backend/src/main/java/com/rungo/api/domain/users/enumtype/Provider.kt
@@ -0,0 +1,8 @@
+package com.rungo.api.domain.users.enumtype
+
+enum class Provider {
+    LOCAL,
+    GOOGLE,
+    KAKAO,
+    NAVER
+}
---
+++ backend/src/main/java/com/rungo/api/domain/users/enumtype/Role.kt
@@ -0,0 +1,7 @@
+package com.rungo.api.domain.users.enumtype
+
+enum class Role {
+    PARTICIPANT,
+    ORGANIZER,
+    ADMIN
+}
