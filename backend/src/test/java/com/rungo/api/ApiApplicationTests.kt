b33053fed5a0a40f784efcd1e98fe3c6b549e5b1
MMuhwon <mwon1590@gmail.com>
2026-05-19 14:03:56 +0900

    [RG-83] refactor: ApiApplication kotlin migration

---
+++ backend/src/test/java/com/rungo/api/ApiApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.rungo.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
